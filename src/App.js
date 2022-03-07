cbbd1b79ad157d547ddbee4ddb4075424429aab3
putrasatr <satriaputrapratama2002@gmail.com>
2022-03-07 11:44:48 +0700

    Simpler Counter SSr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, countSet] = useState(1);
+  const increment = () => {
+    countSet((c) => c + 1);
+  };
+  const decrement = () => {
+    countSet((c) => c - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
   );
 }
 
